ca510fd6a0dc29d4ec8e177d42db30b4d83ac882
yueh0607 <imyueh@foxmail.com>
2023-05-21 17:11:07 +0800

    增加对Unity的异步支持

---
+++ CoEvent/Runtime/Async/UnitySupport/AsyncOperationEx.cs
@@ -0,0 +1,34 @@
+﻿using CoEvent.Async;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoEvent.Async
+{
+#if UNITY_2017_1_OR_NEWER
+    public static class AsyncOperationEx
+    {
+        public static CoTask<AsyncOperation> GetAwaiter(this AsyncOperation operation)
+        {
+            var task = CoTask<AsyncOperation>.Create();
+            operation.completed += task.SetResult;
+            return task;
+
+        }
+        public static AsyncOperation WithProgress(this AsyncOperation operation,Action<float> progress)
+        {
+            Action<float> callback = (x) =>
+            {
+                progress?.Invoke(operation.progress);
+            };
+            operation.completed += (x) =>
+            {
+                CoEvents.Instance.Operator<IUpdate>().UnSubscribe(callback);
+            };
+            CoEvents.Instance.Operator<IUpdate>().Subscribe(callback);
+            return operation;
+        }
+    }
+#endif
+}
